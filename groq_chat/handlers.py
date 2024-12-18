44a85722431971c62068e8b2451fe51d43893080
Plamen Vatev <pazoff@gmail.com>
2024-12-18 11:39:53 +0200

    Update handlers.py
    added llama-3.3-70b-versatile model

--- groq_chat/handlers.py
+++ groq_chat/handlers.py
@@ -111,7 +111,7 @@ async def model_command_handler(
     update: Update, context: ContextTypes.DEFAULT_TYPE
 ) -> None:
     """Change the model used to generate responses."""
-    models = ["llama-3.1-8b-instant", "llama-3.1-70b-versatile", "gemma2-9b-it", "llama3-8b-8192", "llama3-70b-8192", "mixtral-8x7b-32768", "gemma-7b-it"]
+    models = ["llama-3.3-70b-versatile", "llama-3.1-8b-instant", "llama-3.1-70b-versatile", "gemma2-9b-it", "llama3-8b-8192", "llama3-70b-8192", "mixtral-8x7b-32768", "gemma-7b-it"]
 
     reply_markup = InlineKeyboardMarkup(
         [
